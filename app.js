90601551754f2d921ab45681eaadcf4aaa310651
Ashu <ashutoshm90@gmail.com>
2016-02-26 00:37:41 +0530

    init 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Created by ASHUTOSH on 2/25/2016.
  */
-var PORT = process.env.npm_package_config_port || 3000;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
