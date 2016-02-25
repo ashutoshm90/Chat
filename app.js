919055410d31b4e51e1d5cde7f0e0ba29226a2bf
Ashu <ashutoshm90@gmail.com>
2016-02-26 00:10:30 +0530

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ASHUTOSH on 2/25/2016.
+ */
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function(){
+    console.log("Server Started");
+});
