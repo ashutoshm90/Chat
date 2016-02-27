cad8600c0fc77f7e5ce16a3740e633fb2ef3f8a5
Ashu <ashutoshm90@gmail.com>
2016-02-28 02:00:52 +0530

    style join

--- server.js
+++ server.js
@@ -16,10 +16,41 @@ http.listen(PORT, function(){
     console.log("Server Started");
 });
 
+function sendCurrentUsers(socket){
+    var info = clientInfo[socket.id];
+    var users = [];
+    if(typeof info === 'undefined'){
+        return;
+    }
 
+    Object.keys(clientInfo).forEach(function(socketId){
+        var userinfo = clientInfo[socketId];
+        if(info.room === userinfo.room){
+            users.push(userinfo.name);
+        }
+    });
+
+    socket.emit('message', {
+        name: "System",
+        text: "Current Users:" + users.join(', '),
+        timestamp: moment().valueOf()
+    });
+}
 
 io.on('connection', function(socket){
     console.log("user connected");
+    socket.on('disconnect', function(){
+       var userData = clientInfo[socket.id];
+       if (typeof userData !== 'undefined'){
+           socket.leave(userData.room);
+           io.to(userData.room).emit('message', {
+               name: "System",
+               text: userData.name + ' has left',
+               timestamp: moment().valueOf()
+           });
+           delete clientInfo[socket.id];
+       }
+    });
 
     socket.on('joinRoom', function(req){
         socket.join(req.room);
@@ -32,9 +63,15 @@ io.on('connection', function(socket){
     });
     socket.on('message', function(message){
        console.log('Message Recieved: ' + message.text);
-       message.timestamp = moment().valueOf();
+       if(message.text === '@currentUsers'){
+           sendCurrentUsers(socket);
+       }else{
+           message.timestamp = moment().valueOf();
 
-        io.to(clientInfo[socket.id].room).emit('message', message);
+           io.to(clientInfo[socket.id].room).emit('message', message);
+       }
+
+
     });
 
 
